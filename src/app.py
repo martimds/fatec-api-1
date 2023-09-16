b8402461fededede4de99a48df178e1de0b7456d
martimds <martimtimdias@gmail.com>
2023-09-16 20:50:11 -0300

    Alterado Tailwind -> Bootstrap

--- src/app.py
+++ src/app.py
@@ -2,10 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/templates")
+@app.route('/')
 def index():
-	return render_template("index.html")
+    return render_template('index.html')
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
