5064a7c4161183e0756a568de61a56870aff3138
martimds <martimtimdias@gmail.com>
2023-09-28 16:36:49 -0300

    Adicionado estruturas e rotas

--- src/app.py
+++ src/app.py
@@ -3,8 +3,22 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('intro.html')
+
+@app.route('/burndown')
+def burndown():
+    return render_template('burndown.html')
+
+@app.route('/intro')
+def intro():
+    return render_template('intro.html')
+
+@app.route('/sprint')
+def sprint():
+    return render_template('sprint.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
