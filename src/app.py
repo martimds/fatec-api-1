f75f283be5a24a1953df427b20a4d335db7f474d
Gabriel Vasconcelos <93106982+gabrielvascf@users.noreply.github.com>
2023-09-29 10:31:22 -0300

    Add procedural generation of page routes

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -7,20 +8,28 @@ app = Flask(__name__)
 def intro():
     return render_template('intro.html')
 
-@app.route('/burndown')
-def burndown():
-    return render_template('burndown.html')
+# @app.route('/burndown')
+# def burndown():
+#     return render_template('burndown.html')
 
 
-@app.route('/sprint')
-def sprint():
-    return render_template('sprint.html')
+# @app.route('/sprint')
+# def sprint():
+#     return render_template('sprint.html')
 
-@app.route('/productowner')
-@app.route('/po')
-def po():
-    return render_template('po.html')
+# @app.route('/productowner')
+# @app.route('/po')
+# def po():
+#     return render_template('po.html')
 
-
+for element in os.listdir(os.getcwd() + "/src/templates"):
+    pagename = str(element).split(".")[0]
+    if pagename == "intro":continue
+    exec(f'''
+print(element, pagename)
+@app.route("/"+pagename)
+def {pagename}():
+    print("{pagename}", element)
+    return render_template("element")''')
 if __name__ == '__main__':
     app.run(debug=True)
