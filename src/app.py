e876fe45de621f7638fff4135484b835cf7d8af6
gabrielvascf <93106982+gabrielvascf@users.noreply.github.com>
2023-10-03 10:19:55 -0300

    Remove old code and comment current algorithm

--- src/app.py
+++ src/app.py
@@ -8,28 +8,16 @@ app = Flask(__name__)
 def intro():
     return render_template('intro.html')
 
-# @app.route('/burndown')
-# def burndown():
-#     return render_template('burndown.html')
-
-
-# @app.route('/sprint')
-# def sprint():
-#     return render_template('sprint.html')
-
-# @app.route('/productowner')
-# @app.route('/po')
-# def po():
-#     return render_template('po.html')
-
-for element in os.listdir(os.getcwd() + "/src/templates"):
+for element in os.listdir(os.getcwd() + "/src/templates"): # Get elements in template directory
     pagename = str(element).split(".")[0]
     if pagename == "intro":continue
+    # Add element to Flask environment
     exec(f'''
 print(element, pagename)
 @app.route("/"+pagename)
 def {pagename}():
     print("{pagename}", element)
     return render_template("element")''')
+
 if __name__ == '__main__':
     app.run(debug=True)
