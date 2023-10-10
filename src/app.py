26dae6e6aaef1e100a20e7045dd5891949e4443b
martimds <martimtimdias@gmail.com>
2023-10-10 18:47:42 -0300

    Add evaluator implementation

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import werkzeug.exceptions
 import os
 
 app = Flask(__name__)
@@ -8,7 +9,18 @@ app = Flask(__name__)
 def intro():
     return render_template('intro.html')
 
-for element in os.listdir(os.getcwd() + "/src/templates"):
+@app.route('/processar', methods=['POST'])
+def processar_formulario():
+    try:
+        criterios = [request.form[f'criterio{i+1}'] for i in range(7)]
+        criterios = list(map(int, criterios))
+        criterios[-3:] = [c * 2 for c in criterios[-3:]]
+        media_final = round(sum(criterios) / len(criterios),1)
+        return render_template('avaliador.html', media=media_final)
+    except werkzeug.exceptions.BadRequestKeyError:
+        return render_template('avaliador.html', media="Existem campos em branco.")
+
+for element in os.listdir("templates"):
     name = str(element).split(".")[0]
     if name == "intro":continue
     exec(f'''
