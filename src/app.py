2ede3ee3f5db06244d645c5a690a9bc7bea8bd70
martimds <martimtimdias@gmail.com>
2023-10-11 18:27:47 -0300

    Implemented grade texts

--- src/app.py
+++ src/app.py
@@ -15,9 +15,20 @@ def calcular():
         criterios = [int(request.form[f'criterio{i}']) for i in range(1, 8)]
         criterios[0] *= 2
         criterios[-1] *= 2
-        return render_template('avaliador.html', media=sum(criterios))
+        pontuacao = sum(criterios) / 4.5
+        if pontuacao <= 3:
+            return render_template('avaliador.html', media="Muito ruim")
+        elif 3 < pontuacao <= 6:
+            return render_template('avaliador.html', media="Ruim")
+        elif 6 < pontuacao <= 7:
+            return render_template('avaliador.html', media="Aceitável")
+        elif 7 < pontuacao <= 9:
+            return render_template('avaliador.html', media="Bom")
+        elif pontuacao > 9:
+            return render_template('avaliador.html', media="Excelente")
+        
     except werkzeug.exceptions.BadRequestKeyError:
-        return render_template('avaliador.html', media="Existem campos em branco.")
+        return render_template('avaliador.html', media="Existem campos em branco")
 
 for element in os.listdir(os.getcwd() + "/src/templates"):
     name = str(element).split(".")[0]
