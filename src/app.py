5b0c996ff08f0333a6774ba76eb782b1dc9cf743
martimds <martimtimdias@gmail.com>
2023-10-12 19:49:54 -0300

    Código formatado

--- src/app.py
+++ src/app.py
@@ -4,31 +4,34 @@ from werkzeug.exceptions import BadRequestKeyError
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/intro')
-def intro():
-    return render_template('intro.html')
 
-@app.route('/calcular', methods=['POST'])
+@app.route("/")
+@app.route("/intro")
+def intro():
+    return render_template("intro.html")
+
+
+@app.route("/calcular", methods=["POST"])
 def calcular():
     try:
-        criterios = [int(request.form[f'criterio{i}']) for i in range(1, 8)]
+        criterios = [int(request.form[f"criterio{i}"]) for i in range(1, 8)]
         criterios[0] *= 2
         criterios[-1] *= 2
         pontuacao = sum(criterios) / 4.5
         if pontuacao <= 3:
-            return render_template('avaliador.html', media="Muito ruim")
+            return render_template("avaliador.html", media="Muito ruim")
         elif 3 < pontuacao <= 6:
-            return render_template('avaliador.html', media="Ruim")
+            return render_template("avaliador.html", media="Ruim")
         elif 6 < pontuacao <= 7:
-            return render_template('avaliador.html', media="Aceitável")
+            return render_template("avaliador.html", media="Aceitável")
         elif 7 < pontuacao <= 9:
-            return render_template('avaliador.html', media="Bom")
+            return render_template("avaliador.html", media="Bom")
         elif pontuacao > 9:
-            return render_template('avaliador.html', media="Excelente")
-        
+            return render_template("avaliador.html", media="Excelente")
+
     except BadRequestKeyError:
-        return render_template('avaliador.html', media="Existem campos em branco")
+        return render_template("avaliador.html", media="Existem campos em branco")
+
 
 @app.route("/<filename>")
 def templates(filename):
@@ -37,5 +40,6 @@ def templates(filename):
     except TemplateNotFound:
         return "Página não encontrada", 404
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
